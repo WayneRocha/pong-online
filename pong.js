f6d62b9718aeb844ff243949c3fb936b4604e26a
Wayne Rocha <dev.waynerocha@gmail.com>
2021-03-19 00:23:36 -0300

    adicionado movimentos a bolinha
    adicionado movimentos a bolinha e refatorado o mecanismo de teclas

--- pong.js
+++ pong.js
@@ -59,7 +59,7 @@ const gameScreen = {
         width: 20,
         height: 60,
         bot(){
-            let errorChance = Math.random() * 20;
+            let errorChance = Math.floor(Math.random() * 20);
             let errorMargin = ((30 + errorChance) / 100) * gameScreen.ball.positionY
             if ((this.positionY + 60) >= gameScreen.ball.positionY + errorMargin){
                 gameScreen.playerMoviment(this, true, false);
@@ -69,19 +69,33 @@ const gameScreen = {
         }
     },
     ball: {
-        positionX: (canvas.width / 2) - 20,
-        positionY: (canvas.height / 2) - 20,
+        positionX: (canvas.width / 2) - 15,
+        positionY: (canvas.height / 2) - 15,
         width: 15,
-        height: 15
+        height: 15,
+        setBallToDefault(){
+            this.positionX = (canvas.width / 2) - this.width;
+            this.positionY = (canvas.height / 2) - this.height;
+        }
     },
     physics:{
-        playersVelocity: 10,
-        ballVelocity: 20
+        playersVelocityY: 10,
+        ballVelocityX: 3,
+        ballVelocityY: 0.5,
     },
     draw(){
         context.fillStyle = 'black';
+        this.drawScreenDivision();
         this.drawPlayers();
         this.drawBall();
+    },
+    drawScreenDivision(){
+        let divisionsBarY = 0;
+        let spaceBetween = 25;
+        for (let i = 0; i <= 4; i++){
+            context.fillRect((canvas.width / 2) - 10, (divisionsBarY) - 10, 10, 40);
+            divisionsBarY += (40 + spaceBetween);
+        }
     },
     drawPlayers(){
         context.fillRect(
@@ -105,13 +119,39 @@ const gameScreen = {
     playerMoviment(player, up, down){
         if (up){
             if (player.positionY > 0){
-                player.positionY -= this.physics.playersVelocity;
+                player.positionY -= this.physics.playersVelocityY;
             }
         } else if(down) {
             if ((player.positionY + player.height) < canvas.height){
-                player.positionY += this.physics.playersVelocity;
+                player.positionY += this.physics.playersVelocityY;
             }
         }
+    },
+    ballMoviment(){
+        let player1Colision = ((this.ball.positionX + this.ball.width) >= this.player1.positionX) && 
+            ((this.ball.positionY + this.ball.height) >= this.player1.positionY && 
+            (this.ball.positionY) <= (this.player1.positionY + this.player1.height));
+        let player2Colision = (this.ball.positionX <= (this.player2.positionX + this.player2.width) &&
+            (this.ball.positionY + this.ball.height) >= this.player2.positionY &&
+            this.ball.positionY <= (this.player2.positionY + this.player2.height));
+        let buttomColision = (this.ball.positionY + this.ball.height) >= canvas.height;
+        let topColision = this.ball.positionY <= 0;
+
+        if (player1Colision){
+            console.log('player 1 colision');
+            this.physics.ballVelocityX *= -1;
+        } else if (player2Colision) {
+            console.log('player 2 colision');
+            this.physics.ballVelocityX = Math.abs(this.physics.ballVelocityX);
+        }
+        if(buttomColision){
+            this.physics.ballVelocityY *= -1;
+        } else if (topColision) {
+            this.physics.ballVelocityY = Math.abs(this.physics.ballVelocityY);
+        }
+    
+        this.ball.positionX += this.physics.ballVelocityX;
+        this.ball.positionY += this.physics.ballVelocityY;
     }
 }
 let currentScreen = startScreen;
@@ -126,64 +166,74 @@ const gameMod = {
 function gameLoop(){
     whiteScreen.draw();
     currentScreen.draw();
-    if (gameMod["single-player"]){
-        if (keyPressed.key == 13){
-            currentScreen = gameScreen;
-        }
-        else if (keyPressed.key == 38 || keyPressed.key == 87){
-            gameScreen.playerMoviment(gameScreen.player1, true, false);
-        }
-        else if (keyPressed.key == 40 || keyPressed.key == 83){
-            gameScreen.playerMoviment(gameScreen.player1, false, true);
-        }
-        else if (keyPressed.key == 27){
-            currentScreen = startScreen;
-        }
-        keyPressed.key = undefined;
-        gameScreen.player2.bot();
-    } else if (gameMod["multiplayer"]){
-        switch (keyPressed.key){
-            case 13:
-                currentScreen = gameScreen;
-                break;
-            case 38:
-                gameScreen.playerMoviment(gameScreen.player1, true, false);
-                break;
-            case 40:
-                gameScreen.playerMoviment(gameScreen.player1, false, true);
-                break;
-            case 87:
-                gameScreen.playerMoviment(gameScreen.player2, true, false);
-                break;
-            case 83:
-                gameScreen.playerMoviment(gameScreen.player2, false, true);
-                break;
-            case 27:
-                currentScreen = startScreen;
-                break;
-        }
-        keyPressed.key = undefined;
-    } else if (gameMod["multiplayer-online"]){
-        //other keyboards commands
+    if (currentScreen == gameScreen){
+        gameScreen.ballMoviment();
     }
-    
+    keyController();
+
+    function keyController(){
+        if (currentScreen == gameScreen){
+            if (gameMod["single-player"]){
+                if (keyPressed.key == 38 || keyPressed.key == 87){
+                    gameScreen.playerMoviment(gameScreen.player1, true, false);
+                }
+                else if (keyPressed.key == 40 || keyPressed.key == 83){
+                    gameScreen.playerMoviment(gameScreen.player1, false, true);
+                }
+                else if (keyPressed.key == 27){
+                    currentScreen = startScreen;
+                }
+                keyPressed.key = undefined;
+                gameScreen.player2.bot();
+            } else if (gameMod["multiplayer"]){
+                switch (keyPressed.key){
+                    case 38:
+                        gameScreen.playerMoviment(gameScreen.player1, true, false);
+                        break;
+                    case 40:
+                        gameScreen.playerMoviment(gameScreen.player1, false, true);
+                        break;
+                    case 87:
+                        gameScreen.playerMoviment(gameScreen.player2, true, false);
+                        break;
+                    case 83:
+                        gameScreen.playerMoviment(gameScreen.player2, false, true);
+                        break;
+                    case 27:
+                        currentScreen = startScreen;
+                        break;
+                }
+                keyPressed.key = undefined;
+            } else if (gameMod["multiplayer-online"]){
+                //other keyboards commands
+            }
+        }
+        else if (currentScreen == startScreen){
+            if (keyPressed.key == 13) currentScreen = gameScreen;
+        } 
+    }
 }
 
 
-window.addEventListener('keydown', function(e){
-    keyPressed.key = e.keyCode;
-}, true);
-setInterval(() => gameLoop(), 10);
+window.addEventListener('keydown', (e) => keyPressed.key = e.keyCode, true);
+ruffleBallInitialDirection();
+setInterval(() => gameLoop(), 16);
 flashyText();
 
 function flashyText(){
-    let count = 10;
-    timer = setInterval(function(){
+    let count = 0;
+    let timer = setInterval(function(){
         count++;
         if(count % 2 === 1) {
             startScreen.option.letterColor = 'white';
         } else {
             startScreen.option.letterColor = 'black';
         }
+        if (currentScreen == gameScreen) clearInterval(timer);
     }, 500);
 }
+function ruffleBallInitialDirection(){
+    Math.floor(Math.random() * 2) ? gameScreen.physics.ballVelocityX *= -1 : gameScreen.physics.ballVelocityX = Math.abs(gameScreen.physics.ballVelocityX);
+    Math.floor(Math.random() * 2) ? gameScreen.physics.ballVelocityY *= -1 : gameScreen.physics.ballVelocityY = Math.abs(gameScreen.physics.ballVelocityY);
+    gameScreen.physics.ballVelocityY = Math.random() * 1.5;
+};
